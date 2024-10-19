914479e3b38ddd6237673e1eee22cd98e9e3bbb2
jakwesoneil <jakwesoneil@gmail.com>
2024-10-19 19:40:03 +0800

    Update DashboardLayout.tsx

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -52,7 +52,7 @@ export const DashboardLayout: React.FC = () => {
           </Box>
 
           {/* Main Content Area */}
-          <Box className="flex-1 h-full px-3 pt-2 overflow-y-auto bg-zinc-300 border-zinc-950 text-zinc-800 ">
+          <Box className="flex-1 h-full px-3 pt-2 overflow-y-auto bg-zinc-100 border-zinc-950 text-zinc-800 ">
             <AppBreadcrumb />
             <Box className="mt-7 overflow-hidden">
               <Outlet />
