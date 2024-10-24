f399ecca22de76a156071f6a5d42164d5b0edd94
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-10-24 12:29:06 +0800

    admin delete acc

--- src/services/accounts.service.ts
+++ src/services/accounts.service.ts
@@ -65,7 +65,7 @@ export const useAccountsService = () => {
     return await httpClient
       .delete("accounts/" + id)
       .then(() => {
-        toast.info("Account  deleted!");
+        toast.info("Account deleted!");
       })
       .catch((error) => {
         handleApiError(error);
--- src/views/dashboard-admin/accounts/ManageAccountsPage.tsx
+++ src/views/dashboard-admin/accounts/ManageAccountsPage.tsx
@@ -1,20 +1,74 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { Flex, Button, Table, Badge } from "@radix-ui/themes";
+import { Flex, Button, Table, Badge, Dialog, Text, TextField } from "@radix-ui/themes";
 import { PageHeader } from "@/components";
 import { useAccountsService } from "@/services";
 
 const ManageAccountsPage: React.FC = () => {
-  const { getAccounts } = useAccountsService();
+  const { getAccounts, deleteAccount } = useAccountsService();
   const { accountType } = useParams<{ accountType: "teacher" | "student" }>();
 
   const [data, setData] = React.useState<any>([]);
+  const [account, setAccount] = React.useState<any>(null);
+
+  const onShowDetails = (accountDetails: any) => {
+    setAccount(accountDetails);
+  };
+
+  const onDeleteAccount = async (id: number) => {
+    if (confirm("Do you confirm to delete this record?")) {
+      await deleteAccount(id).then(() => {
+        getAccounts(accountType).then((data) => setData(data));
+      });
+    }
+  };
 
   React.useEffect(() => {
     getAccounts(accountType).then((data) => setData(data));
-
-    console.log("getAccounts");
   }, [accountType]);
+
+  const AccountDetails: React.FC<any> = (props) => {
+    return (
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <Button>Edit profile</Button>
+        </Dialog.Trigger>
+
+        <Dialog.Content maxWidth="450px">
+          <Dialog.Title>Edit profile</Dialog.Title>
+          <Dialog.Description size="2" mb="4">
+            Make changes to your profile.
+          </Dialog.Description>
+
+          <Flex direction="column" gap="3">
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Name
+              </Text>
+              <TextField.Root defaultValue="Freja Johnsen" placeholder="Enter your full name" />
+            </label>
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Email
+              </Text>
+              <TextField.Root defaultValue="freja@example.com" placeholder="Enter your email" />
+            </label>
+          </Flex>
+
+          <Flex gap="3" mt="4" justify="end">
+            <Dialog.Close>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </Dialog.Close>
+            <Dialog.Close>
+              <Button>Save</Button>
+            </Dialog.Close>
+          </Flex>
+        </Dialog.Content>
+      </Dialog.Root>
+    );
+  };
 
   return (
     <Flex direction="column" gap="3" className="h-full">
@@ -48,10 +102,10 @@ const ManageAccountsPage: React.FC = () => {
                   <Table.RowHeaderCell className="text-zinc-900">{d.email}</Table.RowHeaderCell>
                   <Table.Cell>
                     <Flex direction="row" gap="2">
-                      <Button className="text-xs" color="blue" variant="classic" disabled>
+                      <Button className="text-xs" color="blue" variant="classic" onClick={() => onShowDetails(d)}>
                         Update
                       </Button>
-                      <Button className="text-xs" color="red" variant="classic" disabled>
+                      <Button className="text-xs" color="red" variant="classic" onClick={() => onDeleteAccount(d.id)}>
                         Delete
                       </Button>
                     </Flex>
